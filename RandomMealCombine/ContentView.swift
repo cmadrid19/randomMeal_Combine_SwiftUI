e3e1db76082a73ffa907f651d6a212c68e688148
Max <maxim.macari.dam@gmail.com>
2020-10-21 10:44:38 +0200

    Initial Commit

---
+++ RandomMealCombine/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  RandomMealCombine
+//
+//  Created by Maxim Macari on 21/10/2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ RandomMealCombine/RandomMealCombineApp.swift
@@ -0,0 +1,17 @@
+//
+//  RandomMealCombineApp.swift
+//  RandomMealCombine
+//
+//  Created by Maxim Macari on 21/10/2020.
+//
+
+import SwiftUI
+
+@main
+struct RandomMealCombineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
